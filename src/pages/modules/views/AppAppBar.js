4fcdf42e89fb566b1683e2a656f6493afcf966e4
EdMasterNava <eduardonava2ms@gmail.com>
2023-03-17 23:13:16 -0700

    Switched to Correct Link

--- src/pages/modules/views/AppAppBar.js
+++ src/pages/modules/views/AppAppBar.js
@@ -1,31 +1,21 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import AppBar from '../components/AppBar';
 import Toolbar from '../components/Toolbar';
 import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
 import './css/AppAppBar.css';
 
 function AppAppBar() {
   return (
     <div>
-      <AppBar position="fixed" sx={{backgroundColor: '#fcda68'}}>
-        <Container >
-          <Toolbar sx={{ display: 'flex', justifyContent: 'space-between'}}>
-            <a href="#intro" className="navName">
-              Eduardo Nava
-            </a>
-            <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
-              <a href="#about" className="navTabs">
-                About Me
-              </a>
-              <a href="#projects" className="navTabs">
-                Projects
-              </a>
-              <a href="#reach" className="navTabs">
-                Reach Me
-              </a>
-            </Box>
-          </Toolbar>
+      <AppBar position="fixed" sx={{backgroundColor: '#fcda68', p: 1}}>
+        <Container sx={{display: 'flex', justifyContent: 'space-between'}}>
+          <Link to="/" className="navName">
+            Eduardo Nava
+          </Link>
+          <Link to="/projects/" className="navTabs">
+            Projects
+          </Link>
         </Container>
       </AppBar>
       <Toolbar />
