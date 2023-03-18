b0e4b986f27c809e6826f98aab17ee35aabc5867
EdMasterNava <eduardonava2ms@gmail.com>
2023-03-17 23:09:46 -0700

    Added Routes to Main App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import ProjectLandingPage from './pages/ProjectLandingPage';
+import ProjectHome from './pages/ProjectHome';
 
 function App() {
   return (
     <>
-    <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/" element={<ProjectHome />}></Route>
+        <Route path="/projects/landing-page" element={<ProjectLandingPage />} />
+      </Routes>
     </>
   );
 }
