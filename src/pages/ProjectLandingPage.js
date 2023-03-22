396b39ba3791ffba0ff661576ab4bba1df335d4e
EdMasterNava <eduardonava2ms@gmail.com>
2023-03-21 17:19:10 -0700

    Added Project Components

--- src/pages/ProjectLandingPage.js
+++ src/pages/ProjectLandingPage.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import AppAppBar from './modules/views/AppAppBar';
+import ProjectLandingPageHero from './modules/views/projectViews/ProjectLandingPageHero';
+import ProjectLandingPageShowcase from './modules/views/projectViews/ProjectLandingPageShowcase';
 import Footer from './modules/views/Footer';
 import withRoot from './modules/withRoot';
 
@@ -7,6 +9,8 @@ function ProjectLandingPage() {
     return (
       <React.Fragment>
         <AppAppBar />
+        <ProjectLandingPageHero />
+        <ProjectLandingPageShowcase />
         <Footer />
       </React.Fragment>
     );
