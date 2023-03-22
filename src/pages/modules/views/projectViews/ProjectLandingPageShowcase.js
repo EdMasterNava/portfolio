0121ee22665197e2e398192cc98eba0176702190
EdMasterNava <eduardonava2ms@gmail.com>
2023-03-21 18:06:45 -0700

    Deleted Unused iframe tag

--- src/pages/modules/views/projectViews/ProjectLandingPageShowcase.js
+++ src/pages/modules/views/projectViews/ProjectLandingPageShowcase.js
@@ -6,19 +6,7 @@ function ProjectLandingPageShowcase() {
     return(
         <Box sx={{bgcolor: '#fcda68', py: 10}}>
             <Container>
-                {/* <iframe height="750"  
-                        width="100%" 
-                        scrolling="yes" 
-                        title="FCC Landing Page 'Board Games'" 
-                        src="https://cdpn.io/eddiebuckets/fullembedgrid/ExooJvo?animations=run&type=embed" 
-                        frameborder="0" 
-                        loading="lazy" 
-                        allowtransparency="true" 
-                        allowfullscreen="true" 
-                        sandbox="allow-forms allow-modals allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
-                        data-src="https://cdpn.io/eddiebuckets/fullembedgrid/ExooJvo?animations=run&type=embed">
-                </iframe> */}
-                <iframe height="750" width="100%" src="https://edmasternava.github.io/bjj-reg/" title="FCC Landing Page 'Board Games'"/>
+                <iframe height="750" width="100%" src="https://edmasternava.github.io/board-games-LP/" title="FCC Landing Page 'Board Games'"/>
             </Container>
         </Box>
     )
