bbf49039724ed6c593f2537652b7f9b24f90806a
EdMasterNava <eduardonava2ms@gmail.com>
2023-03-25 00:42:59 -0700

    Added Project Clock to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import ProjectLandingPage from './pages/ProjectLandingPage';
 import ProjectHome from './pages/ProjectHome';
+import ProjectLandingPage from './pages/ProjectLandingPage';
+import ProjectClock from './pages/ProjectClock';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/projects/" element={<ProjectHome />}></Route>
         <Route path="/projects/landing-page" element={<ProjectLandingPage />} />
+        <Route path="/projects/25-5-clock" element={<ProjectClock />} />
       </Routes>
     </>
   );
